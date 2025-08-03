495eda2ef109917554dcc3a31aa775d23a9255b7
jpan8-ai <panj2001@gmail.com>
2025-08-03 10:25:16 -0400

    Sets up the site layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { Nav, Stack } from 'react-bootstrap';
+
+const axiosInstance = Axios.create()
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stack>
+          <Stack direction="horizontal" style={{justifyContent: "space-between", borderRadius: "5px", 
+            background: "linear-gradient(to right, #c6f78eff, #5e996dff)", height: "100px"}}>
+            <div>1</div>
+            <div>Jie Pan's Personal Website</div>
+            <div>3</div>
+          </Stack>
+          <Nav
+            activeKey="/home"
+            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+          >
+            <Nav.Item>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href='/'>About Me</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Stack>
       </header>
-      <main>
+      <main className="Main-Style">
         <h1>Welcome to My App</h1>
         <p>This is a simple React application.</p>
       </main>
