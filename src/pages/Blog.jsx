614144ca1ec818b06129df0b59d32d8eedcd5a9d
jpan8-ai <panj2001@gmail.com>
2026-02-06 11:29:54 -0500

    Use custom Carousel instead of react-bootstrap

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,35 +3,63 @@ import { Link } from 'react-router-dom'
 import './Blog.css'
 import axios from "axios";
 import { useState, useEffect } from 'react';
-import { Carousel, Stack } from "react-bootstrap";
+import Carousel from "../components/Carousel";
 
 export default function Blog() {
   const axiosInstance = axios.create({
-        baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL
-    });
-    const [news, setNews] = useState([]);
-console.log(import.meta.env.REACT_APP_API_BASE_URL);
-    useEffect(() => {
-        axiosInstance.get("getnews?searchterms=ai+OR+artificial+intelligent")
-          .then(response => {
-            console.log(response);
-            let news = [];
-            for(var i = 0; i < response?.data?.length; i += 3)
+      baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL
+  });
+  const [news, setNews] = useState([]);
+  const [carouselItems, setCarouselItems] = useState([]);
+
+  useEffect(() => {
+    axiosInstance.get("getnews?searchterms=ai+OR+artificial+intelligent")
+      .then(response => {
+        console.log(response);
+        let news = [];
+        for(var i = 0; i < response?.data?.length; i += 2)
+        {
+            let inner = [];
+            for(var j = i; j < i + 2 && j < response?.data?.length; j ++)
             {
-                let inner = [];
-                for(var j = i; j < i + 3 && j < response?.data?.length; j ++)
-                {
-                    inner = [...inner, response?.data[j]];
-                }
-                news = [...news, inner];
+                inner = [...inner, response?.data[j]];
             }
-            setNews(news);
-            console.log(news);
-          })
-          .catch(err => 
-              console.log(err)
-          );
-    }, []);
+            news = [...news, inner];
+        }
+        setNews(news);
+      })
+      .catch(err => 
+          console.log(err)
+      );
+  }, []);
+
+  useEffect(() => {
+    var tempCarItems = []
+    news.forEach((value, index) => {
+      tempCarItems = [...tempCarItems, 
+        (
+          <div className="posts-grid">
+            {value.map((newsItem, index) =>        
+                <article key={index} className="post-card card-hover">
+                  <div className="post-meta">
+                    <span className="post-date">{newsItem.publish_date}</span>
+                    <span className="post-read">{newsItem.author}</span>
+                  </div>
+                  <h2 className="post-title">
+                    {newsItem.title}
+                  </h2>
+                  <Link to={newsItem.url} className="post-link" target='_blank'>
+                    Read More →
+                  </Link>
+                </article>                     
+            )} 
+          </div>       
+        )
+      ]
+    });
+    setCarouselItems(tempCarItems);
+    console.log(tempCarItems);
+  }, [news])
 
   return (
     <motion.main 
@@ -47,30 +75,8 @@ console.log(import.meta.env.REACT_APP_API_BASE_URL);
         AI and Software Development News; thoughts on AI and Software Development, and everything in between.
       </p>
 
-      <Carousel variant="dark">
-        {news.map((value, index) =>
-          <Carousel.Item>
-            1
-            {/* <div className="posts-grid">
-              {value.map((news, index) =>        
-                  <article key={index} className="post-card card-hover">
-                    <div className="post-meta">
-                      <span className="post-date">{news.publish_date}</span>
-                      <span className="post-read">{news.author}</span>
-                    </div>
-                    <h2 className="post-title">
-                      {news.title}
-                    </h2>
-                    {/* <p className="post-excerpt">{news.text}</p>
-                    <Link to={news.url} className="post-link">
-                      Read More →
-                    </Link>
-                  </article>                     
-              )} 
-            </div>*/}
-          </Carousel.Item>
-        )}
-      </Carousel>
+      <Carousel items={carouselItems}/>
+      <div style={{height: "100px"}}></div>
 
       <div className="newsletter">
         <h3>Stay Updated</h3>
